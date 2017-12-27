53bea3998225047542a28c5b8c68b5ae11f3db74
Ekaterina <e.v.kasilova@gmail.com>
2017-12-27 19:08:00 +0300

    Delete AreaAvr_Config.R

--- AreaAvr_Config.R
+++
@@ -1,7 +0,0 @@
-rm(list=ls())
-#CMIP5_dir_name<-"D://_CFD Data_//CERA_Data//ETHr4 experiment//tas_4-5//"
-#CMIP5_dir_name<-"D://_CFD Data_//CERA_Data//ETHr4 experiment//pr_4-5//"
-CMIP5_dir_name<-"D://_CFD Data_//CERA_Data//ETHr4 experiment//mrro_4-5//"
-rd_name<-"D://_CFD Data_//Data Processing//Results//Gridded//"
-wd_name<-"D://_CFD Data_//Data Processing//_Runoff_//"
-setwd(wd_name)
