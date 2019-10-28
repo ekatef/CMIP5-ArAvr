d9846bfaa7ba6b9eaa1fdb3bcc17aaad1840a159
Ekaterina <e.v.kasilova@gmail.com>
2019-10-28 12:56:51 +0300

    Minor changes

--- 0_ProcessCMIP5_Config.R
+++ 0_ProcessCMIP5_Config.R
@@ -1,24 +1,44 @@
-x_RU_big <- 180
-x_RU_small <- -20
-y_RU_big <- 75
-y_RU_small <- 40
-
-current_param <- "sfcWind"
+current_param <- "tas"
 it_is_temterature <- (current_param %in% "tas")
 
-exp_code <- 45
+mod_set_id <- "good_for_all_names"
 
+#-----------
+# future
+#-----------
 YearsRange_check_0 <- 2007:2016
-YearsRange_check <- 2085:2094
+YearsRange_check <- 2045:2054
 
-# months to be taken for seasonal calculations
+exp_code <- 45
+exp_id <- paste0("rcp", exp_code)
+
+#*********************************
+# process all years: calculates seasonal mean 
+# for each model and the set years
+# & redridn the result on the "standard" mesh
 SeasonsSet <- c(1L:12L)
+#*********************************
+x_RU_big <- 180
+x_RU_small <- -20
 
-mod_set_id <- "ens_max"
+y_RU_big <- 80
+y_RU_small <- 40
+#------------------------------------------------------------------------------
 
+#***********************************
+
+#------------------------------------------------------------------------------
+#		model set definition
+#------------------------------------------------------------------------------
 mod_set_names <- list(
 	test = c("MPI-ESM-LR", "CMCC-CMS"),
-	ens_max = "ALL")
+	ens_max = "ALL",
+	recom_Carvalho8 = c("MPI-ESM-LR", "MPI-ESM-MR",
+						"HadGEM2-CC","HadGEM2-ES",
+						"CMCC-CMS", "CNRM-CM5",
+						"GFDL-CM3", "ACCESS1-0"),
+)
+
 mod_config_names <- names(mod_set_names)
 
 if (!(mod_set_id %in% mod_config_names)) {
@@ -28,6 +48,5 @@ if (!(mod_set_id %in% mod_config_names)) {
 		paste(mod_config_names, collapse = ", "))
 	)
 }
-selected_names_to_proc <- mod_set_names[[mod_set_id]]
 
-exp_id <- paste0("rcp", exp_code)
+selected_names_to_proc <- mod_set_names[[mod_set_id]]
